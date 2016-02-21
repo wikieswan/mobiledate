bb71015ce85191d0a4b2ca1e6ed7232e36e171f6
wikies.wan <1175551641@qq.com>
2016-02-21 21:17:33 +0800

    new enginer

--- js/m-date.js
+++ js/m-date.js
@@ -47,6 +47,11 @@ $(function(){
 			$($(dom).find('.md-row')[index]).removeClass('md-row-0').removeClass('md-row-1').removeClass('md-row-2')
 		}
 	}
+	function getTargetTopNumber(target){
+		var top = $(target).css('top');
+		top  = util.px2number(top)
+		return top;
+	}
 	var yYearStart,timeYearStart;
 	$('body').on('touchstart','.md-col-mask',function(e){
 		var touch = e.changedTouches[0]
@@ -61,6 +66,7 @@ $(function(){
 		if($(this).siblings('.md-col-items').hasClass('md-year')){
 			yMove = touch.clientY;
 			timeMove = e.timeStamp;
+			touchMoveSlide('.md-year',yMove-yYearStart)
 		}
 	})
 	$('body').on('touchend','.md-col-mask',function(e){
@@ -70,8 +76,67 @@ $(function(){
 			yEnd = touch.clientY;
 			timeEnd = e.timeStamp;
 		}
-		console.log(e,yEnd-yYearStart)
+		
+		var y0 = getTargetTopNumber('.md-year');
+		var sMinus = touchEndMinusLength(timeEnd,timeYearStart,yEnd,yYearStart)
+		y0 = y0 + sMinus;
+
+		touchEndSlide('.md-year',y0);
 	})
+
+
+	function touchEndMinusLength(timeEnd,timeStart,yTouchEnd,yTouchStart){
+		var aMinus = 0.005;
+		var t = timeEnd - timeStart,
+			s = yTouchEnd - yTouchStart,
+			v = 2*s/t,
+			vEnd = 2*s/t,
+			sMinus = v*v/2/aMinus;
+		if(v>0){
+			//dowm
+
+		} 
+		else{
+			//up
+			sMinus = -1 * sMinus 
+		}
+		return sMinus;
+	}
+
+	function touchEndSlide(target,y){
+		var targetHeight = util.px2number($(target).css('height'))
+		var n = targetHeight/40;
+		if(y<(4-n)*40){
+			y = (4-n)*40
+		}
+		else if(y>3*40){
+			y = 3*40
+		}
+		y = Math.round(y/40)*40;
+		$(target).css({
+			'transition': 'top 1s',
+			'-webkit-transition': 'top 1s',
+			'top': y
+		});
+		
+	}
+
+	function touchMoveSlide(target,y){
+		var y0 = getTargetTopNumber(target)
+		var ymove = y0+y/10;
+		var targetHeight = util.px2number($(target).css('height'))
+		var n = targetHeight/40;
+		if(ymove<(3.5-n)*40||ymove>3.5*40){
+			return false;
+		}
+		
+		$(target).css({
+			'transition': 'top 0s',
+			'-webkit-transition': '0s',
+			'top': ymove
+		})
+	}
+
 
 	initYear()
 	function initYear(year){
@@ -84,10 +149,23 @@ $(function(){
 		for(var i=0;i<60;i++){
 			yearHtml += '<div class="md-item">'+ (baseYear+i) +'</div>'
 		}
-		$('.md-year').css('top',(1970-year+3)*40);
+		
 		$('.md-year').html(yearHtml);
+		$('.md-year').css('top',(1970-year+3)*40);
+		
 	}
-
+	function straightMoveEngine(target,y){
+		var y0 = getTargetTopNumber(target)
+		var ymove = y0+y/10;
+		var targetHeight = util.px2number($(target).css('height'))
+		var n = targetHeight/40;
+		if(ymove<(3.5-n)*40||ymove>3.5*40){
+		}
+		else{
+			touchMoveSlide(target,ymove)
+		}
+	}
+	
 })
 
 
